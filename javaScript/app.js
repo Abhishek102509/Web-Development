33d426398759146d3e87fe42ef28570c06aa8700
Mohit <mrai645@gmail.com>
2023-10-03 00:41:32 +0530

    Create app.js

---
+++ javaScript/app.js
@@ -0,0 +1,68 @@
+// let a=5;
+// let b=4;
+
+// console.log(a+b);
+
+// prompt("set your password")
+
+// let msg = "       ha     ";
+// console.log(msg);
+// console.log(msg.trim());
+
+// let str = "rhrd";
+
+// console.log(str.toUpperCase());
+
+// let msg = "ILoveCoding";
+// console.log(msg.indexOf("love"));
+
+// let str = "       hello    ";
+
+// console.log(str.trim().toUpperCase());
+// let str = "apple";
+// console.log(str[1,4]);
+
+// let students = ["aaaaa", "bbbbbb" , "ggggg"]
+// console.log(students);
+
+// let car = ["audi","rr","bmw"];
+
+// car.unshift("lamborghini");
+// console.log(car);
+
+// car.shift();
+// console.log(car);
+
+// let months = ["janurary","june","march","august"];
+
+// // let days = ["monday","tuesday"];
+
+// console.log(months.splice(0,2,"black","grey"));
+
+// months.shift();
+// months.shift();
+
+// months.unshift("june");
+// months.unshift("july");
+// console.log(months);
+
+// let num = ["monday","tuesday","sunday","wednessday"];
+// num.sort(function(a,b){return b-a});
+// console.log(num);
+
+// num.sort();
+// console.log(num);
+
+// let months = ["janurary","july","march","august","november"];
+
+// months.splice(0,1);
+// months.splice(1,3,"june");
+// months.splice(0,2,"july","june");
+// console.log(months);
+
+let arr =  ["c","c++","html","javascript","python","java","c#","sql"];
+arr.reverse();
+console.log(arr);
+
+arr.indexOf("javascript");
+console.log(arr);
