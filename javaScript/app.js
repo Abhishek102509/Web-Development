6fa5c20aefdb87c4b023dac414f4b5cd586f7f7b
Mohit <mrai645@gmail.com>
2023-10-21 22:28:17 +0530

    then& catch

--- javaScript/app.js
+++ javaScript/app.js
@@ -284,31 +284,59 @@
 
 // Setting up the promises
 
-function savetoDb(data, sucess, failure){
-    let internetSpeed = Math.floor(Math.random() *10) + 1;
-    if(internetSpeed > 4) {
-        sucess();
+// function savetoDb(data, sucess, failure){
+//     let internetSpeed = Math.floor(Math.random() *10) + 1;
+//     if(internetSpeed > 4) {
+//         sucess();
 
-    } else {
-        failure();
-    }
+//     } else {
+//         failure();
+//     }
+// }
+
+// savetoDb("apna college", () => {
+//     console.log("sucess : your data was saved");
+//     savetoDb( "hello world", () => {
+//         console.log("sucess2 : data2 saved");
+//         savetoDb( "mohit", () => {
+//             console.log("sucess3 : data3 saved");
+//         },
+//         () => {
+//             console.log("failure3: weak connection");
+//         });
+//     },
+//     () => {
+//         console.log("failure2 : weak connection");
+//     });
+// },
+//  () => {
+//     console.log("failure3 : weak connection");
+//  });
+
+h1 = document.querySelector("h1");
+
+function changeColor(color, delay) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            h1.style.color = color;
+            resolve("color changed!");
+        }, delay);
+    });
 }
 
-savetoDb("apna college", () => {
-    console.log("sucess : your data was saved");
-    savetoDb( "hello world", () => {
-        console.log("sucess2 : data2 saved");
-        savetoDb( "mohit", () => {
-            console.log("sucess3 : data3 saved");
-        },
-        () => {
-            console.log("failure3: weak connection");
-        });
-    },
-    () => {
-        console.log("failure2 : weak connection");
-    });
-},
- () => {
-    console.log("failure3 : weak connection");
- });
+changeColor("red", 1000)
+.then(() => {
+    console.log("red color was completed");
+    return changeColor("orange", 1000);
+})
+.then(() => {
+    console.log("orange color was completed");
+    return changeColor("green", 1000);
+})
+.then(() => {
+    console.log("green color was completed");
+    return changeColor("blue", 1000);
+})
+.then(() => {
+    console.log("blue color was completed");
+});
