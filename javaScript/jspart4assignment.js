6c0a294394e02a3e6231934eaf32fc8e78883cc6
Mohit <mrai645@gmail.com>
2023-10-24 16:49:10 +0530

    splice

---
+++ javaScript/jspart4assignment.js
@@ -0,0 +1,15 @@
+//write ajs program to delete all occurrence of element 'num' in a given array
+// if arr = [1,2,3,4,5,6,2,3] & num=2
+let arr=[1,2,3,4,5,6,2,3];
+let num = 2;
+
+for(let i=0;i<arr.length;i++){
+    
+
+    if(arr[i]==num){
+        arr.splice(i,1);
+    }
+
+  
+}
+console.log(arr);
